7b3b748b533a1892a47eb800054fe0284947447f
Malladi Ravindra <malladiravindra1@gmail.com>
2026-09-24 20:12:17 +0530

    fix: add vercel frontend to CORS_ALLOWED_ORIGINS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -130,7 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = env_list(
     'CORS_ALLOWED_ORIGINS',
-    'http://localhost:5173,http://127.0.0.1:5173,'
+    'http://localhost:5173,http://127.0.0.1:5173,https://ravindra-sable.vercel.app'
 )
 
 
